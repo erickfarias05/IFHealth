173165401759309ce0a3385c106a56de0543b732
Erick Farias <79072531+erickfarias05@users.noreply.github.com>
2022-03-05 21:06:54 +0000

    Finalização do manter esportes

--- Parte02/main.cs
+++ Parte02/main.cs
@@ -2,13 +2,21 @@ using System;
 
 class Program{
   public static void Main() {
-    Console.WriteLine("Bem-vindo ao IFHealth");
+    Console.WriteLine("Seja bem-vindo ao IFHealth");
     int op = 0;
     do {
+      try {
       op = Menu();
-      switch(op) {
-        case 1 : EsporteInserir(); break;
-        case 2 : EsporteListar(); break;
+        switch(op) {
+          case 1 : EsporteInserir(); break;
+          case 2 : EsporteListar(); break;
+          case 3 : EsporteAtualizar(); break;
+          case 4 : EsporteExcluir(); break;
+        }
+      }
+      catch (Exception erro) {
+        op = -1;
+        Console.WriteLine("Erro: " + erro.Message);
       }
     } while (op != 0);
   }
@@ -17,6 +25,8 @@ class Program{
     Console.WriteLine("------ Faça sua escolha! ------");
     Console.WriteLine("01 - Inserir um novo esporte à academia");
     Console.WriteLine("02 - Listar os esportes cadastrados da academia");
+    Console.WriteLine("03 - Atualizar os dados de algum esporte da academia");
+    Console.WriteLine("04 - Excluir um esporte da academia");
     Console.WriteLine("00 - Finalizar o sistema");
     Console.WriteLine("-------------------------------");
     Console.Write("Opção: ");
@@ -39,5 +49,24 @@ class Program{
     foreach(Esporte objeto in Sistema.EsporteListar())
       Console.WriteLine(objeto);
     Console.WriteLine("----------------------------");
+  }
+  public static void EsporteAtualizar() {
+    Console.WriteLine("-------- Atualizar dados de um esporte --------");
+    Console.Write("Informe o id do esporte: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Informe o novo nome do esporte: ");
+    string nome = Console.ReadLine();
+    Esporte objeto = new Esporte(nome, id);
+    Sistema.EsporteAtualizar(objeto);
+    Console.WriteLine("------ Operação concluída! ------");
+  }
+    public static void EsporteExcluir() {
+    Console.WriteLine("-------- Excluir um esporte --------");
+    Console.Write("Informe o id do esporte: ");
+    int id = int.Parse(Console.ReadLine());
+    string nome = "";
+    Esporte objeto = new Esporte(nome, id);
+    Sistema.EsporteExcluir(objeto);
+    Console.WriteLine("------ Operação concluída! ------");
   }
 }
--- Parte02/sistema.cs
+++ Parte02/sistema.cs
@@ -14,8 +14,33 @@ class Sistema {
   }
   public static Esporte[] EsporteListar() {
     //Retornar objetos
-    Esporte[] alo = new Esporte[nEsporte];
-    Array.Copy(esportes, alo, nEsporte);
-    return alo;
+    Esporte[] mud = new Esporte[nEsporte];
+    Array.Copy(esportes, mud, nEsporte);
+    return mud;
+  }
+  public static Esporte EsporteListar(int id) {
+    foreach(Esporte objeto in esportes)
+      if (objeto != null && objeto.GetId() == id) return objeto;
+    return null;
+  }
+  public static void EsporteAtualizar(Esporte objeto) {
+    Esporte mud = EsporteListar(objeto.GetId());
+    if (mud != null)
+      mud.SetNome(objeto.GetNome());
+  }
+  public static void EsporteExcluir(Esporte objeto) {
+    int mud = EsporteIndice(objeto.GetId());
+    if (mud != -1) {
+      for (int i = mud; i < nEsporte - 1; i++)
+        esportes[i] = esportes[i + 1];
+      nEsporte--;
+    }
+  }
+  private static int EsporteIndice(int id) {
+    for (int i = 0; i < nEsporte; i++) {
+      Esporte objeto = esportes[i];
+      if (objeto.GetId() == id) return i;
+    }
+    return -1;
   }
 }
